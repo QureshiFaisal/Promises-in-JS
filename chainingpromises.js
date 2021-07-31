3b76a403453b9a26eb6060f67c3bfd534655dd73
QureshiFaisal <85436772+QureshiFaisal@users.noreply.github.com>
2021-07-31 19:13:03 +0530

    Update chainingpromises.js

--- chainingpromises.js
+++ chainingpromises.js
@@ -1,5 +1,25 @@
 'use strict'
 
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function(data){
+const html = ` <article class="country">
+<img class="country__img" src="${data.flag}" />
+<div class="country__data">
+  <h3 class="country__name">${data.name}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+</div>
+</article>
+`;
+
+countriesContainer.insertAdjacentHTML('beforeend',html);
+countriesContainer.style.opcacity = 1;
+});
+};
     //Country 1
     const getCountryData = function(country){
     fetch(`https://restcountries.eu/rest/v2/name/${country}`)
