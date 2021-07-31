13b5f8f9ea4721334d63f9559ccd7109672a6fff
QureshiFaisal <85436772+QureshiFaisal@users.noreply.github.com>
2021-07-31 20:15:46 +0530

    first commit

--- handlingerrorsindividually.js
+++ handlingerrorsindividually.js
@@ -1,10 +1,15 @@
 
 'use strict'
 
-//Country 1
+const renderError = function(msg){
+countriesContainer.insertAdjacentText('beforeend', msg);
+countriesContainer.style.opacity = 1;
+
+}// This function will display the error to the user.
 const getCountryData = function(country){
+    //Country 1
 fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-.then(response=>response.json(), err => alert(err))// if there is an error the second function will display it.
+.then(response=>response.json())
 .then(data=>{
     renderCountry(data[0]);
     const neighbour = data[0].borders[0];
@@ -14,8 +19,14 @@ fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     //Country 2
     return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
 })
-.then(response => response.json(), err=> alert(err))// if there is an error and the promise is not fulfilled this will display the error.
-.then(data => renderCountry(data));
-
+.then(response => response.json())
+.then(data => renderCountry(data))
+.catch(err => {
+    console.error(`${err}`)// this catch method will catch any error that occurs throughout the promise chain. Thus it is better then handling errors individually for each promise.
+       renderError(`Something went wrong. ${err.message}. Try again`);// This string will be passed as an arguement to the function on top.
+       //Error in java script are objects and have a message property. We will access it to ensure that only the error is dosplayed to the user and not the whole object.
+}).finally(() => {
+    countriesContainer.style.opacity = 1;
+});// the then method is executed only when the promise is fulfilled, catch is executed only when the promise is rejected due to an error. Finally is executed in both cases. So if you want some code to run in both cases put it in finally.
 };
 getCountryData('portugal');
